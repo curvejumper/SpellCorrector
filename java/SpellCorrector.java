1f7aba952566b9e9f42b8f0dbb21a13d5b245f00
Michael Ortiz <ortiz.d.michael@gmail.com>
2016-04-25 14:12:14 -0700

    Formatting

--- java/SpellCorrector.java
+++ java/SpellCorrector.java
@@ -53,6 +53,7 @@ public class SpellCorrector {
 
     private ArrayList<String> edits(String word) {
         ArrayList<String> result = new ArrayList<>();
+        
         for (int i = 0; i < word.length(); ++i) {
             //adds substrings of the word **will not work well, gives word a
             //a chance to be many other, incorrect, choices
@@ -61,6 +62,7 @@ public class SpellCorrector {
             //Removes single characters to see if that is a popular word
             result.add(word.substring(0, i) + word.substring(i + 1));
         }
+        
         for (int i = 0; i < word.length() - 1; ++i) {
             //Removes two characters
 //            result.add(word.substring(0, i) + word.substring(i + 2));
@@ -68,12 +70,14 @@ public class SpellCorrector {
             //Switches two characters to see if more popular word
             result.add(word.substring(0, i) + word.substring(i + 1, i + 2) + word.substring(i, i + 1) + word.substring(i + 2));
         }
+        
         //replaces a letter **making it look more like another word, thus, increases incorrectness
 //        for (int i = 0; i < word.length(); i++) {
 //            for (char c = 'a'; c <= 'z'; c++) {
 //                    result.add(word.substring(0, i) + String.valueOf(c) + word.substring(i + 1));
 //            }
 //        }
+
         //adds an extra letter
         for (int i = 0; i <= word.length(); ++i) {
             for (char c = 'a'; c <= 'z'; c++) {
